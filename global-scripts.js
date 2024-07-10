193f8cf00c3735a5342cba0a92dc87405ff70d5e
lottoland-sandbox <168644089+lottoland-sandbox@users.noreply.github.com>
2024-07-10 10:32:48 +0200

    Update global-scripts.js

--- global-scripts.js
+++ global-scripts.js
@@ -1,1 +1,117 @@
+// ** FIELD TRACKING - ONLY FIRST FOCUS IN
+function trackOnFieldFocus(event) {
+    console.log('Field focused');
+    var target = event.target;
+    // Check if the focused element has a data-track attribute
+    if (target.hasAttribute('data-track-name')) {
+        // Extract attributes prefixed with "data-track-name"
+        
+        var trackContainer = target.getAttribute('data-track-container') || '';
+        var trackObject = target.getAttribute('data-track-object') || '';
+        var trackName = target.getAttribute('data-track-name');
+        var trackValue = target.getAttribute('data-track-value');
+        var trackCategory = target.getAttribute('data-track-category') || '';
+        var trackSubcategory = target.getAttribute('data-track-subcategory') || '';
 
+        // Construct event properties object
+        var event_properties = {
+            Name: trackName,
+            Container: trackContainer,
+            Object: trackObject,
+            Category: trackCategory,
+            Value: 'firstfocusin',
+        };
+
+        // Fire amplitude tracking with 'focus' event type
+        event_properties['Feature'] = Feature;
+        event_properties['Domain'] = Domain; 
+        amplitude.track('click', event_properties);
+    }
+}
+
+// Attach focus event listener to all form fields with data-track attribute
+document.addEventListener('focusin', function(event) {
+    var target = event.target;
+    if (target.tagName === 'INPUT' && target.hasAttribute('data-track-name')) {
+        trackOnFieldFocus(event);
+    }
+});
+
+
+// ** FUNCTION TO TRACK LINK CLICKS
+function trackLinkClick(event) {
+    var target = event.target;
+    if (target.tagName === 'A' && target.hasAttribute('data-track-name')) {
+        var trackContainer = target.getAttribute('data-track-container') || '';
+        var trackObject = target.getAttribute('data-track-object') || '';
+        var trackName = target.getAttribute('data-track-name');
+        var trackValue = target.getAttribute('data-track-value');
+        var trackCategory = target.getAttribute('data-track-category') || '';
+        var trackSubcategory = target.getAttribute('data-track-subcategory') || '';
+        var trackGenre = target.getAttribute('data-track-genre') || '';
+        var trackProduct = target.getAttribute('data-track-product') || '';
+        var trackDescription = target.getAttribute('data-track-description') || '';
+
+        // Construct event properties object
+        var event_properties = {
+            Name: trackName,
+            Container: trackContainer,
+            Object: trackObject,
+            Category: trackCategory,
+            Genre: trackGenre,
+            Product: trackProduct,
+            Description: trackDescription,
+        };
+
+        // Fire amplitude tracking with 'click' event type
+        event_properties['Feature'] = Feature;
+        event_properties['Domain'] = Domain; 
+        amplitude.track('click', event_properties);
+    }
+}
+
+// Attach click event listener to all links with data-track-name attribute
+var links = document.querySelectorAll('a[data-track-name]');
+links.forEach(function(link) {
+    link.addEventListener('click', trackLinkClick);
+});
+
+
+// ** FUNCTION TO TRACK BUTTON CLICKS
+function trackButtonClick(event) {
+    var target = event.target;
+    if (target.tagName === 'BUTTON' && target.hasAttribute('data-track-name')) {
+        var trackContainer = target.getAttribute('data-track-container') || '';
+        var trackObject = target.getAttribute('data-track-object') || '';
+        var trackName = target.getAttribute('data-track-name');
+        var trackValue = target.getAttribute('data-track-value') || '';
+        var trackCategory = target.getAttribute('data-track-category') || '';
+        var trackSubcategory = target.getAttribute('data-track-subcategory') || '';
+    
+
+        // Construct event properties object
+        var event_properties = {
+            Name: trackName,
+            Container: trackContainer,
+            Object: trackObject,
+            Category: trackCategory,
+            Value: trackValue,
+            Campaign: trackCampaign,
+            Offer: trackOffer,
+            Genre: trackGenre,
+            Product: trackProduct,
+            Description: trackDescription,
+        };
+
+        // Fire amplitude tracking with 'click' event type
+        event_properties['Feature'] = Feature;
+        event_properties['Domain'] = Domain; 
+        amplitude.track('click', event_properties);
+    }
+}
+
+// Attach click event listener to all buttons with data-track-name attribute
+var buttons = document.querySelectorAll('button[data-track-name]');
+buttons.forEach(function(button) {
+    button.addEventListener('click', trackButtonClick);
+});
