1c13e875e80397ad8a49e371b08ebf84fc2bb53e
lottoland-sandbox <168644089+lottoland-sandbox@users.noreply.github.com>
2024-07-17 09:24:58 +0100

    Update global-scripts.js

--- global-scripts.js
+++ global-scripts.js
@@ -276,13 +276,55 @@ buttons.forEach(function(button) {
             return payload;
         }
 
-function amp(event,eventPayload){
+function amp(event,eventPayload,userPayload){
     let userAgentString = navigator.userAgent;
     var globalPayload=globalProperties(userAgentString);
-    var payload=mergePayloads(eventPayload,globalPayload);
+    var payload=mergePayloads(eventPayload,globalPayload,userPayload);
     amplitude.track(event, payload); 
 }
      
-function mergePayloads(payload1, payload2) {
-    return Object.assign({}, payload1, payload2);
+function mergePayloads(payload1, payload2,payload3) {
+    return Object.assign({}, payload1, payload2, payload3);
+}
+
+
+
+//RANDOM USERS
+var user1 = {
+    'name': 'MJ',
+    'Account Status': 'VIP',
+    'Verification Status': 'verified',
+    'Player Number': '1234',
+    'Balance': '10.23',
+};
+
+var user2 = {
+    'name': 'ML',
+    'Account Status': 'suspended',
+    'Verification Status': 'unverified',
+    'Player Number': '4321',
+    'Balance': '102.23',
+};
+
+var user3 = {
+    'name': 'PG',
+    'Account Status': 'closed',
+    'Verification Status': 'unverified',
+    'Player Number': '2222',
+    'Balance': '0.22',
+};
+
+var user4 = {
+    'name': 'JF',
+    'Account Status': 'self excluded',
+    'Verification Status': 'pending',
+    'Player Number': '18882',
+    'Balance': '9.11',
+};
+
+// Function to select a random payload
+function getRandomPlayer() {
+    const payloads = [user1, user2, user3, user4];
+    const randomIndex = Math.floor(Math.random() * payloads.length);
+    return payloads[randomIndex];
 }
